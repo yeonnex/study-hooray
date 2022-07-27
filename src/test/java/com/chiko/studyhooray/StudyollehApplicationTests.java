2c06beae627b52a0bf60af41ae9efec65d9a8fb5
yeonnex <yeonnex@gmail.com>
2022-07-27 22:43:22 +0900

    Add Account domain class

---
+++ src/test/java/com/chiko/studyhooray/StudyollehApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chiko.studyhooray;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyollehApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
