1b60f30676587abe9b73c6640d4f3d80e28250fa
yeonnex <yeonnex@gmail.com>
2022-08-07 19:47:53 +0900

    temp commit

---
+++ src/main/java/com/chiko/studyhooray/model/Notifications.java
@@ -0,0 +1,26 @@
+package com.chiko.studyhooray.model;
+
+import com.chiko.studyhooray.domain.Account;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class Notifications {
+    private boolean studyCreatedByEmail;
+    private boolean studyCreatedByWeb;
+    private boolean studyEnrollmentResultByEmail;
+    private boolean studyEnrollmentResultByWeb;
+    private boolean studyUpdatedByEmail;
+    private boolean studyUpdatedByWeb;
+    public Notifications(Account account) {
+        this.studyCreatedByEmail = account.isStudyCreatedByEmail();
+        this.studyCreatedByWeb = account.isStudyCreatedByWeb();
+        this.studyEnrollmentResultByEmail = account.isStudyEnrollmentResultByEmail();
+        this.studyEnrollmentResultByWeb = account.isStudyEnrollmentResultByWeb();
+        this.studyUpdatedByEmail = account.isStudyUpdatedByEmail();
+        this.studyUpdatedByWeb = account.isStudyUpdatedByWeb();
+    }
+}
